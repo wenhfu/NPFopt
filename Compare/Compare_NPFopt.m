1c2821a77201cb27a50df8173276b3c82e4aaaab
wenhfu <wenhfu@163.com>
2025-10-06 22:53:17 +0800

    2025.10.06

--- Compare/Compare_NPFopt.m
+++ Compare/Compare_NPFopt.m
@@ -8,33 +8,33 @@ cd('..')
 addpath(pwd);
 cd(pwd_compare)
 MF_all = [0,2,4,6];
+% MF_all = 2;
+
 %%
 for MF_idx=1:length(MF_all)
     MF = MF_all(MF_idx);
     %% Comparision for c(x)=0
     Test_Problem = 'Eq';
-    con_type = 0;
     pwd_Problem = fullfile(pwd_compare,'All_Problems/',Test_Problem);
     cd(pwd_Problem)
     SubFolderNames = GetFolders(pwd)';
     N = length(SubFolderNames);
     TP_index = 1:N;
     Data_NPFoptEq = zeros(N,6);
+    % for Loop_i = setdiff(TP_index,[51 52 53 54])
     for Loop_i = TP_index
+    % for Loop_i = [11    24    37    47      54    65    86    98   105   106   108   109   122  ]  % 51 52 53
         fprintf('\nComparision for c(x)=0,  MF=%1d, Problem Name%9s:  ',MF,SubFolderNames{Loop_i});
         Dir = fullfile(pwd_Problem,SubFolderNames{Loop_i});
         cd(Dir)
         try prob=cutest_setup();catch;cutest_terminate();prob=cutest_setup();end
-        % x0=prob.x; funf=@(x) cutest_obj(x); func=@(x) cutest_cons(x); 
-        % [x,fval,exitflag,output,lambda] = NPFopt_main(funf,func,con_type,x0,MF);
         x0=prob.x; funf=@(x) cutest_obj(x); func=@(x) nonlcon(x,prob); 
-        [x,fval,exitflag,output,lambda] = NPFopt_main(funf,func,con_type,x0,MF);
+        [x,fval,exitflag,output,lambda] = NPFopt_new(funf,func,x0,MF);
         cutest_terminate();
         Data_NPFoptEq(Loop_i,1:6) = [fval,output.con,exitflag,output.iter,output.nf,output.ngf];
     end
     %% Comparision for c(x)>0
     Test_Problem = 'IneqGe';
-    con_type = 1;
     pwd_Problem = fullfile(pwd_compare,'All_Problems/',Test_Problem);
     cd(pwd_Problem)
     SubFolderNames = GetFolders(pwd)';
@@ -42,18 +42,18 @@ for MF_idx=1:length(MF_all)
     TP_index = 1:N;
     Data_NPFoptIneqGe = zeros(N,6);
     for Loop_i=TP_index
+    % for Loop_i = setdiff(TP_index,[169]-123)
+    % for Loop_i= [ 138   146   147   148   149   150     173   174  ] - 123  % 169
         fprintf('\nComparision for c(x)>0,  MF=%1d, Problem Name%9s:  ',MF,SubFolderNames{Loop_i});
         Dir = fullfile(pwd_Problem,SubFolderNames{Loop_i});
         cd(Dir)
         try prob=cutest_setup();catch;cutest_terminate();prob=cutest_setup();end
-        % funf=@(x) cutest_obj(x); func=@(x) cutest_cons(x); x0=prob.x;
         x0=prob.x; funf=@(x) cutest_obj(x); func=@(x) nonlcon(x,prob); 
-        [x,fval,exitflag,output,lambda] = NPFopt_main(funf,func,con_type,x0,MF);
+        [x,fval,exitflag,output,lambda] = NPFopt_new(funf,func,x0,MF);
         Data_NPFoptIneqGe(Loop_i,1:6) = [fval,output.con,exitflag,output.iter,output.nf,output.ngf];
     end
     %% Comparision for c(x)<0
     Test_Problem = 'IneqLe';
-    con_type = -1;
     pwd_Problem = fullfile(pwd_compare,'All_Problems/',Test_Problem);
     cd(pwd_Problem)
     SubFolderNames = GetFolders(pwd)';
@@ -61,21 +61,21 @@ for MF_idx=1:length(MF_all)
     TP_index = 1:N;
     Data_NPFoptIneqLe = zeros(N,6);
     for Loop_i=TP_index
+    % for Loop_i = [ 180   194   196 ] - 175
         fprintf('\nComparision for c(x)<0,  MF=%1d, Problem Name%9s:  ',MF,SubFolderNames{Loop_i});
         Dir = fullfile(pwd_Problem,SubFolderNames{Loop_i});
         cd(Dir)
         try prob=cutest_setup();catch;cutest_terminate();prob=cutest_setup();end
-        % funf=@(x) cutest_obj(x); func=@(x) cutest_cons(x); x0=prob.x;
         x0=prob.x; funf=@(x) cutest_obj(x); func=@(x) nonlcon(x,prob); 
-        [x,fval,exitflag,output,lambda] = NPFopt_main(funf,func,con_type,x0,MF);
+        [x,fval,exitflag,output,lambda] = NPFopt_new(funf,func,x0,MF);
         Data_NPFoptIneqLe(Loop_i,1:6) = [fval,output.con,exitflag,output.iter,output.nf,output.ngf];
     end
     Data_NPFopt{MF_idx} = [Data_NPFoptEq;Data_NPFoptIneqGe;Data_NPFoptIneqLe];
 end
 cd(pwd_compare);
 
-save Data_NPFopt Data_NPFopt
-
+% save Data_NPFopt Data_NPFopt
+ 
 %% Comparision with Lancelot
 load Data_NPFopt
 load('All_Problems_Lan/Lancelot_data_Eq.mat')
@@ -97,12 +97,6 @@ Nf_MF4 = Data_NPFopt{3}(:,5);
 Ng_MF4 = Data_NPFopt{3}(:,6);
 Nf_MF6 = Data_NPFopt{4}(:,5);
 Ng_MF6 = Data_NPFopt{4}(:,6);
-Idx_large=[];
-for i=1:N
-    if Nf_MF0(i)==0 && Ng_MF0(i)==0
-        Idx_large=union(Idx_large,i);
-    end
-end
 %% Print to screan
 fprintf('No.    name     n    m   |  lan: f       nf   ng  flag | MF0: f         nf  ng  flag |  MF2: f       nf   ng  flag |  MF4: f       nf   ng  flag |  MF6: f       nf   ng  flag\n');
 for i=1:N
@@ -145,7 +139,7 @@ for i=1:N
             '%4d/%4d/%4d&', ...
             '%4d/%4d/%4d \\\\ \n'], ...
            i,Data_Lancelot_flag{i,3},Data_Lancelot(i,1:2),Data_Lancelot(i,3:4),Data_NPFopt{1}(i,[5,6,4]),Data_NPFopt{2}(i,[5,6,4]),Data_NPFopt{3}(i,[5,6,4]),Data_NPFopt{4}(i,[5,6,4]));
-    elseif ~isempty(setdiff(i,Idx_large))
+    else
         fprintf(fid_output_failed_MF0,['%3d&%9s&%4d&%4d&' ...
             '%4d/%4d&' ...
             '%4d/%4d/%4d&', ...
@@ -162,7 +156,7 @@ for i=1:N
             '%4d/%4d/%4d&', ...
             '%4d/%4d/%4d \\\\ \n'], ...
            i,Data_Lancelot_flag{i,3},Data_Lancelot(i,1:2),Data_Lancelot(i,3:4),Data_NPFopt{1}(i,[5,6,4]),Data_NPFopt{2}(i,[5,6,4]),Data_NPFopt{3}(i,[5,6,4]),Data_NPFopt{4}(i,[5,6,4]));
-    elseif ~isempty(setdiff(i,Idx_large))
+    else
         fprintf(fid_output_failed_MF6,['%3d&%9s&%4d&%4d&' ...
             '%4d/%4d&' ...
             '%4d/%4d/%4d&', ...
@@ -183,69 +177,72 @@ fclose(fid_output_failed_MF6);
 IdxFailed_Lan = []; IdxFailed_MF0 = []; IdxFailed_MF2 = []; IdxFailed_MF4 = []; IdxFailed_MF6 = [];
 n_fail_lan = 0; n_fail_MF0 = 0; n_fail_MF2 = 0; n_fail_MF4 = 0; n_fail_MF6 = 0;
 for i=1:N
-    if Data_Lancelot(i,5)~=0 && ~isempty(setdiff(i,Idx_large))
+    if Data_Lancelot(i,5)~=0
         Nf_Lan(i) = Inf; Ng_Lan(i) = Inf; IdxFailed_Lan=union(IdxFailed_Lan,i); n_fail_lan=n_fail_lan+1;
     end
-    if Data_NPFopt{1}(i,3)<=0 && ~isempty(setdiff(i,Idx_large))
+    if Data_NPFopt{1}(i,3)<=0
         Nf_MF0(i) = Inf; Ng_MF0(i) = Inf; IdxFailed_MF0=union(IdxFailed_MF0,i);n_fail_MF0=n_fail_MF0+1;
     end
-    if Data_NPFopt{2}(i,3)<=0 && ~isempty(setdiff(i,Idx_large))
+    if Data_NPFopt{2}(i,3)<=0
         Nf_MF2(i) = Inf; Ng_MF2(i) = Inf; IdxFailed_MF2=union(IdxFailed_MF2,i);n_fail_MF2=n_fail_MF2+1;
     end
-    if Data_NPFopt{3}(i,3)<=0 && ~isempty(setdiff(i,Idx_large))
+    if Data_NPFopt{3}(i,3)<=0
         Nf_MF4(i) = Inf; Ng_MF4(i) = Inf; IdxFailed_MF4=union(IdxFailed_MF4,i);n_fail_MF4=n_fail_MF4+1;
     end
-    if Data_NPFopt{4}(i,3)<=0 && ~isempty(setdiff(i,Idx_large))
+    if Data_NPFopt{4}(i,3)<=0
         Nf_MF6(i) = Inf; Ng_MF6(i) = Inf; IdxFailed_MF6=union(IdxFailed_MF6,i);n_fail_MF6=n_fail_MF6+1;
     end
 end
 Nf_min = min([Nf_Lan,Nf_MF0,Nf_MF2,Nf_MF4,Nf_MF6]')';
 Ng_min = min([Ng_Lan,Ng_MF0,Ng_MF2,Ng_MF4,Ng_MF6]')';
-Idx = setdiff(1:N,Idx_large);
-N_output = length(Idx);
-Nf_min=Nf_min(Idx);
-Ng_min=Ng_min(Idx);
 clear P_Nf_Lan P_Ng_Lan P_Nf_MF0 P_Ng_MF0 P_Nf_MF2 P_Ng_MF2 P_Nf_MF4 P_Ng_MF4 P_Nf_MF6 P_Ng_MF6
 k=1;
-axis_tau = 1:0.5:25;
+axis_tau = 1:0.01:25;
 for tau=axis_tau
-    P_Nf_Lan(k) = (sum((Nf_Lan(Idx)./Nf_min)<tau)/N_output);
-    P_Ng_Lan(k) = (sum((Ng_Lan(Idx)./Ng_min)<tau)/N_output);
-    P_Nf_MF0(k) = (sum((Nf_MF0(Idx)./Nf_min)<tau)/N_output);
-    P_Ng_MF0(k) = (sum((Ng_MF0(Idx)./Ng_min)<tau)/N_output);
-    P_Nf_MF2(k) = (sum((Nf_MF2(Idx)./Nf_min)<tau)/N_output);
-    P_Ng_MF2(k) = (sum((Ng_MF2(Idx)./Ng_min)<tau)/N_output);
-    P_Nf_MF4(k) = (sum((Nf_MF4(Idx)./Nf_min)<tau)/N_output);
-    P_Ng_MF4(k) = (sum((Ng_MF4(Idx)./Ng_min)<tau)/N_output);
-    P_Nf_MF6(k) = (sum((Nf_MF6(Idx)./Nf_min)<tau)/N_output);
-    P_Ng_MF6(k) = (sum((Ng_MF6(Idx)./Ng_min)<tau)/N_output);
+    P_Nf_Lan(k) = (sum((Nf_Lan./Nf_min)<=tau)/N);
+    P_Ng_Lan(k) = (sum((Ng_Lan./Ng_min)<=tau)/N);
+    P_Nf_MF0(k) = (sum((Nf_MF0./Nf_min)<=tau)/N);
+    P_Ng_MF0(k) = (sum((Ng_MF0./Ng_min)<=tau)/N);
+    P_Nf_MF2(k) = (sum((Nf_MF2./Nf_min)<=tau)/N);
+    P_Ng_MF2(k) = (sum((Ng_MF2./Ng_min)<=tau)/N);
+    P_Nf_MF4(k) = (sum((Nf_MF4./Nf_min)<=tau)/N);
+    P_Ng_MF4(k) = (sum((Ng_MF4./Ng_min)<=tau)/N);
+    P_Nf_MF6(k) = (sum((Nf_MF6./Nf_min)<=tau)/N);
+    P_Ng_MF6(k) = (sum((Ng_MF6./Ng_min)<=tau)/N);
     k=k+1;
 end
 
 fig = figure;
 fig.Position = [655 283 600 300];
 subplot(1,2,1)
-stairs(axis_tau,P_Nf_Lan,'LineWidth',1)
+stairs(axis_tau,P_Nf_Lan,'LineWidth',1.5)
 hold on
-stairs(axis_tau,P_Nf_MF0,'LineWidth',1)
-stairs(axis_tau,P_Nf_MF2,'LineWidth',1)
-stairs(axis_tau,P_Nf_MF4,'LineWidth',1)
-stairs(axis_tau,P_Nf_MF6,'LineWidth',1)
+stairs(axis_tau,P_Nf_MF0,'LineWidth',1.5)
+stairs(axis_tau,P_Nf_MF2,'LineWidth',1.5)
+stairs(axis_tau,P_Nf_MF4,'LineWidth',1.5)
+stairs(axis_tau,P_Nf_MF6,'LineWidth',1.5)
 legend('LANCELOT','MF=0','MF=2','MF=4','MF=6','Location','Southeast')
-axis([2,tau,0.3,0.9])
+axis([1,tau,0.2,0.9])
 title('nf/best nf')
 
 subplot(1,2,2)
-stairs(axis_tau,P_Ng_Lan,'LineWidth',1)
+stairs(axis_tau,P_Ng_Lan,'LineWidth',1.5)
 hold on
-stairs(axis_tau,P_Ng_MF0,'LineWidth',1)
-stairs(axis_tau,P_Ng_MF2,'LineWidth',1)
-stairs(axis_tau,P_Ng_MF4,'LineWidth',1)
-stairs(axis_tau,P_Ng_MF6,'LineWidth',1)
+stairs(axis_tau,P_Ng_MF0,'LineWidth',1.5)
+stairs(axis_tau,P_Ng_MF2,'LineWidth',1.5)
+stairs(axis_tau,P_Ng_MF4,'LineWidth',1.5)
+stairs(axis_tau,P_Ng_MF6,'LineWidth',1.5)
 legend('LANCELOT','MF=0','MF=2','MF=4','MF=6','Location','Southeast')
 title('ngf/best ngf')
-axis([2,tau,0.3,0.9])
+axis([1,tau,0.2,0.9])
 print(fig,'Fig.eps','-depsc2','-r600')
+
+N_succ = N - [n_fail_lan;n_fail_MF0;n_fail_MF2;n_fail_MF4;n_fail_MF6];
+fprintf('Number of problems solved for Lancelot: %3d\n',N-n_fail_lan);
+fprintf('Number of problems solved when MF=0: %3d\n',N-n_fail_MF0);
+fprintf('Number of problems solved when MF=2: %3d\n',N-n_fail_MF2);
+fprintf('Number of problems solved when MF=4: %3d\n',N-n_fail_MF4);
+fprintf('Number of problems solved when MF=6: %3d\n',N-n_fail_MF6);
 
 %% GetFolderNames
 function [SubFolders] = GetFolders(ParentFolder)
