5e5ffcc5e209d632682d4b36ea8125d90b893400
wenhfu <152390872+wenhfu@users.noreply.github.com>
2025-10-27 09:07:12 +0800

    2025.10.27

--- NPFopt.m
+++ NPFopt.m
@@ -329,6 +329,7 @@ if alg_display == 1
     end
     fprintf('n =%4d, meq =%4d, mineq =%4d, nf =%4d, ng =%4d, iter =%3d, f =%12.4e, v =%12.4e ',n,m_eq,m_ineq,nf,ngf,iter,fxnew,vxnew);
 end
+fprintf('\n');
 x = xnew; fval = fxnew; exitflag = flag_Alg;
 output.con = vxnew;
 output.iter = iter;
